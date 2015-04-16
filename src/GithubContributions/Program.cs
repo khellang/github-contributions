e993d8cde290cb0879b91ec47f40a0f6358de860
Kristian Hellang <kristian@hellang.com>
2015-04-16 21:08:50 +0200

    Initial commit

---
+++ src/GithubContributions/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace GithubContributions
+{
+    public static class Program
+    {
+        private static readonly Regex RepoNameRegex = new Regex(
+            @"^https://api\.github\.com/repos/(?<repo>[a-zA-Z0-9.-]+/[a-zA-Z0-9.-]+)/issues/\d+$");
+
+        public static int Main(string[] args)
+        {
+            return MainAsync(args).Result;
+        }
+
+        private static async Task<int> MainAsync(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: github-contributions <author>");
+                return 1;
+            }
+
+            var information = new ProductHeaderValue("github-contributions");
+
+            var client = new GitHubClient(information);
+
+            var searchTerm = string.Format("is:merged is:pr author:{0}", args[0]);
+
+            var pullRequests = await FetchPullRequests(client.Search, searchTerm);
+
+            var lookup = pullRequests
+                .ToLookup(GetRepositoryName)
+                .OrderByDescending(x => x.Count());
+
+            WriteResults(lookup);
+
+            return 0;
+        }
+
+        private static void WriteResults(IOrderedEnumerable<IGrouping<string, Issue>> lookup)
+        {
+            Console.WriteLine("Number of repositories: {0}", lookup.Count());
+            Console.WriteLine("Number of pull requests: {0}", lookup.Sum(x => x.Count()));
+
+            Console.WriteLine();
+
+            foreach (var group in lookup)
+            {
+                Console.WriteLine("{0}: {1}", @group.Key, @group.Count());
+            }
+        }
+
+        private static async Task<List<Issue>> FetchPullRequests(ISearchClient client, string searchTerm)
+        {
+            var pullRequests = new List<Issue>();
+
+            var page = 1;
+
+            SearchIssuesResult result;
+
+            do
+            {
+                var request = new SearchIssuesRequest(searchTerm) { Page = page++ };
+
+                result = await client.SearchIssues(request);
+
+                pullRequests.AddRange(result.Items);
+            } while (pullRequests.Count < result.TotalCount);
+
+            return pullRequests;
+        }
+
+        private static string GetRepositoryName(Issue issue)
+        {
+            return RepoNameRegex.Match(issue.Url.ToString()).Groups["repo"].Value;
+        }
+    }
+}
---
+++ src/GithubContributions/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GithubContributions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kristian Hellang")]
+[assembly: AssemblyProduct("GithubContributions")]
+[assembly: AssemblyCopyright("Copyright © Kristian Hellang 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4d36a5ed-cdc6-497d-9ce6-90d03ecdf311")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
