ae4d4fbcde5c25324966e8abb1096d8f8ab781c1
Kristian Hellang <kristian@hellang.com>
2015-06-26 10:39:13 +0200

    Added Costura, removed 'is:merged'

--- src/GithubContributions/Program.cs
+++ src/GithubContributions/Program.cs
@@ -29,7 +29,7 @@ namespace GithubContributions
 
             var client = new GitHubClient(information);
 
-            var searchTerm = string.Format("is:merged is:pr author:{0}", args[0]);
+            var searchTerm = string.Format("is:pr author:{0}", args[0]);
 
             var pullRequests = await FetchPullRequests(client.Search, searchTerm);
 
